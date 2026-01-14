04e3c3b620988603b09e07f6065cc9d2d20d4d77
aiden <1098003693@qq.com>
2026-01-14 10:53:09 +0800

    -

---
+++ js/swt.js
@@ -0,0 +1,185 @@
+document.writeln("<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Transitional//EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\'>");
+document.writeln("<html xmlns=\'http://www.w3.org/1999/xhtml\'>");
+document.writeln("<meta http-equiv=\'X-UA-Compatible\' content=\'IE=EmulateIE8\' />");
+document.writeln("<head>");
+document.writeln("<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'>");
+document.writeln("</head>");
+document.writeln("<body>");
+
+document.writeln('<style type="text/css">');
+document.writeln('#LRdiv1,#LRdiv3,#LXB_CONTAINER_SHOW,#LRfloater0{display:none !important;}');
+document.writeln("</style>");
+
+var url=window.location.href;
+// if(url.indexOf("/mtbd/") > 0 || url.indexOf("/yltd/") > 0 || url.indexOf("/zxhd/") > 0 || url.indexOf("/ymgs/") > 0 || url.indexOf("/xshy/") > 0 || url.indexOf("/jpks/etbj/") > 0 || url.indexOf("/jobs/") > 0 || url.indexOf("/angel/xfjs/") > 0 || url.indexOf("/angel/xfaq/") > 0 || url.indexOf("/angel/tfya/") > 0 || url.indexOf("/angel/blzd/") > 0 || url.indexOf("/angel/dflz/") > 0 || url.indexOf("/angel/jkjy/") > 0 || url.indexOf("/jpks/") > 0 || url.indexOf("/complaint/") > 0 || url.indexOf("/yjy/") > 0 || url.indexOf("/hlfc/") > 0){
+//
+// }else{
+	document.writeln("<style>");
+	document.writeln(".nr_conter img {display:none !important}");
+	document.writeln("</style>");
+// }
+
+
+document.writeln("<script language=\"javascript\" src=\"http://mlk.zoosnet.net/JS/LsJS.aspx?siteid=MLK16844568&float=1&lng=cn\"></script>");
+function swt_dhaaaaa(obj){
+var ins=$(obj).parents("div").attr("id");
+var swt="http://mlk.zoosnet.net/LR/Chatpre.aspx?id=MLK16844568&lng=cn&r="+document.referrer+"#swt&p="+window.location.href;
+window.open(swt);
+}//引用结束(此处应用第三方的聊天工具)
+
+function swt_hm()
+{
+LR_HideInvite();
+openZoosUrl();
+return false;
+}
+
+
+
+
+
+function openM(){
+	$("#swt").fadeIn();
+}
+
+$(document).ready(function(){
+	
+setTimeout("openM()",10000);
+	$("#close").click(function(){
+		$("#swt").fadeOut();
+		setTimeout("openM()",86400*1000);
+	});					
+						
+}); 
+
+
+
+function swt(){
+openZoosUrl();LR_HideInvite();return false;
+}
+(function(){
+	var ls= document.getElementsByTagName("a");
+	for(var i=0;i<ls.length;i++){
+		if(/javascript:swt/i.test(ls[i].href)){
+			ls[i].href="javascript:;";
+			ls[i].onclick=function(){return swt();};
+		}
+	}
+})();
+
+ var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+      var browser = navigator.userAgent.toLowerCase();
+      var isMobile = false;
+      for (var i=0; i<mobileAgent.length; i++){ if (browser.indexOf(mobileAgent[i])!=-1){ isMobile = true;
+//alert(mobileAgent[i]);
+          location.href = 'http://wap.tianyunhos.com/';
+          break; } }
+/*Left*/
+
+document.writeln("<style>");
+document.writeln("#xf{");
+document.writeln("	position: fixed;");
+document.writeln("	left:0px;");
+document.writeln("	top: 50%;");
+document.writeln("	margin-top: -257px;");
+document.writeln("	width: 92px;");
+document.writeln("	height: 425px;");
+document.writeln("	background-repeat: no-repeat;");
+document.writeln("	background-position: center;");
+document.writeln("	background-image: url(/images/0left.png);");
+document.writeln("	z-index: 989;");
+document.writeln("	padding: 38px 16px 0;");
+document.writeln("");
+document.writeln("}");
+document.writeln("");
+document.writeln(".xf1 dl{");
+document.writeln("	margin:0;");
+document.writeln("}");
+document.writeln("");
+document.writeln(".xf1 dd{");
+document.writeln("	padding: 15px 0;");
+document.writeln("}");
+document.writeln(".xf1 dd>a{");
+document.writeln("	display: block;");
+document.writeln("	font-size: 12px;");
+document.writeln("	color: #333;");
+document.writeln("	text-align: center;");
+document.writeln("}");
+document.writeln(".xf1 dd>a:hover{");
+document.writeln("	color: #33767e;");
+document.writeln("}");
+document.writeln(".xf1 dd>a img{");
+document.writeln("	display: block;");
+document.writeln("	width: 40px;");
+document.writeln("	margin:0 auto 7px;");
+document.writeln("}");
+document.writeln(".xf1 dd+dd{");
+//document.writeln("	border-top: 1px solid #eee;");
+document.writeln("}");
+document.writeln(".xf2{");
+document.writeln("	margin-top: 10px; display:none");
+document.writeln("}");
+document.writeln(".xf2 a,.xf2 img{");
+document.writeln("	display: block;margin:0 auto; cursor: pointer;");
+document.writeln("}");
+document.writeln(".wzdt-item{");
+document.writeln("	padding-top: 30px;");
+document.writeln("}");
+document.writeln(".wzdt-item a:hover{");
+document.writeln("	font-weight: bold;");
+document.writeln("	color: #42a098;");
+document.writeln("}");
+document.writeln(".wzdt-h1{");
+document.writeln("	font-size: 20px;");
+document.writeln("	color: #333;");
+document.writeln("	padding-bottom: 10px;");
+document.writeln("	border-bottom: 1px solid #ddd;");
+document.writeln("}");
+document.writeln("");
+document.writeln(".wzdt-dl dl{");
+document.writeln("	margin:0;");
+document.writeln("}");
+document.writeln("");
+document.writeln(".wzdt-dl dd{");
+document.writeln("	float: left;");
+document.writeln("	margin-right: 30px;");
+document.writeln("	margin-top: 18px;");
+document.writeln("	font-size: 15px;");
+document.writeln("	color: #42a098;");
+document.writeln("}");
+document.writeln("#main4-list22{");
+document.writeln("	margin-top: 30px;");
+document.writeln("}");
+document.writeln("</style>");
+
+
+document.writeln("<div id=\'xf\'>");
+document.writeln("	<div class=\'xf1\'>");
+document.writeln("		<dl>");
+document.writeln("			<dd><a href=\'/angel/xfjs/\'><img src=\'/images/left1.png\' alt=\'\'>行风建设</a></dd>");
+document.writeln("			<dd><a href=\'/angel/dflz/ddjs/\'><img src=\'/images/left2.png\' alt=\'\'>党风廉政</a></dd>");
+document.writeln("			<dd><a href=\'/angel/yydz/\'><img src=\'/images/left3.png\' alt=\'\'>来院交通</a></dd>");
+document.writeln("			<dd><a href=\'/yltd/zjtime/\'><img src=\'/images/left4.png\' alt=\'\'>专家出诊表</a></dd>");
+document.writeln("		</dl>");
+document.writeln("	</div>");
+document.writeln("	<div class=\'xf2\'><a id=\'totop\'><img src=\'/images/left5.png\' alt=\'\'></a></div>");
+document.writeln("</div>");
+
+
+/*反腐倡廉*/
+document.writeln('<div id="divpiao2" style="z-index: 99999999999; right: 0; visibility: visible; width: 341px; position: fixed; bottom: 0; height: 235px; "><div id="submenu"><div id="menuTitl" onclick="showsubmenu()"><img  src="/images/close.jpg" style="cursor: pointer;" width="50" height="20" border="0" vspace="3" alt="关闭广告"></div><a  href="/angel/mtbd/4535.html"  target="_blank"> <img  src="/images/corruption.jpg"  width="341" height="205" border="0" alt=""></a></div></div>');
+document.writeln("<script type=\'text/javascript\'>");
+document.writeln("");
+document.writeln("");
+document.writeln("function showsubmenu() {");
+document.writeln("");
+document.writeln("// eval(\'submenu\' +\'.style.display=\'none\';\');");
+document.writeln("$(\'#divpiao2\').hide();");
+document.writeln("}");
+
+document.writeln('$("#totop").click(function(){');
+document.writeln("		$('html, body').animate({ scrollTop: 0 }, 500);");
+document.writeln("})");
+
+document.writeln("</script>");
+
